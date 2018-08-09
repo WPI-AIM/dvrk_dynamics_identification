dfced52b60c01892ab81b227ee1e8ee29d4d49b6
Radian Azhar <radiang91@gmail.com>
2018-08-09 15:17:50 -0400

    Cant pickle Lambidify

--- robot_model_data.py
+++ robot_model_data.py
@@ -8,12 +8,11 @@ class RobotModel:
         self.dof = dyn.rbt_def.dof
         self.coordinates = dyn.rbt_def.coordinates
         self.T_0n = dyn.geom.T_0n
-
         self.base_num = dyn.base_num
         self.base_param = dyn.base_param
-        self.H_b_func = dyn.H_b_func
+        self.std_param = dyn.rbt_def.std_params
+        self.bary_param = dyn.rbt_def.bary_params
 
-        self.bary_param = dyn.bary_params
-        self.H_func = dyn.H_func
-
-        self.std_param = dyn.std_params
+        #Lambdify functions have problems
+        #self.H_b_func = dyn.H_b_func
+        #self.H_func = dyn.H_func
