9eee8ec9375aaaad8ed7cb18ff33b3c60f1e1945
Radian Azhar <radiang91@gmail.com>
2018-08-09 22:13:45 -0400

    T_0n, numpy.Matrix, problem loading

--- robot_model_data.py
+++ robot_model_data.py
@@ -7,12 +7,14 @@ class RobotModel:
     def __init__(self, dyn):
         self.dof = dyn.rbt_def.dof
         self.coordinates = dyn.rbt_def.coordinates
-        self.T_0n = dyn.geom.T_0n
         self.base_num = dyn.base_num
         self.base_param = dyn.base_param
         self.std_param = dyn.rbt_def.std_params
         self.bary_param = dyn.rbt_def.bary_params
 
-        #Lambdify functions have problems
+        #Problems Loading
+        self.T_0n = dyn.geom.T_0n #Matrix??
+
+        #Problems Dumping
         #self.H_b_func = dyn.H_b_func
         #self.H_func = dyn.H_func
