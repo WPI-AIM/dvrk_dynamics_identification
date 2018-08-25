44ed1491ab70dc7c8a6443fedb448c303e416e71
Radian Azhar <radiang91@gmail.com>
2018-08-25 19:50:04 -0400

    try

--- run_robot.py
+++ run_robot.py
@@ -42,7 +42,8 @@ else:
     p.move_joint_some(a[0, 0:dof], array)
 print(array)
 
-states = np.zeros((len(q), 3 * dof))
+#states = np.zeros((len(q), 3 * dof))
+states = np.zeros((len(q), 2 * dof))
 
 print(states.shape)
 i = 0
@@ -51,22 +52,30 @@ while i < len(a) and not rospy.is_shutdown():
         p.move_joint_some(a[i, 0:dof-1], array, False)
         p.move_jaw(a[0, -1],False)
 
+        # states[i][0:dof-1] = p.get_current_joint_position()[0:dof-1]
+        # states[i][7] = p.get_current_jaw_position()
+        # states[i][dof:dof * 2-1] = p.get_current_joint_velocity()[0:dof-1]
+        # states[i][dof+7] = p.get_current_jaw_velocity()
+        # states[i][dof * 2:dof * 3-1] = p.get_current_joint_effort()[0:dof-1]
+        # #states[i][2 * dof + 7] = p.get_current_jaw_effort()
+        # #print('it works')
+
         states[i][0:dof-1] = p.get_current_joint_position()[0:dof-1]
         states[i][7] = p.get_current_jaw_position()
-        states[i][dof:dof * 2-1] = p.get_current_joint_velocity()[0:dof-1]
-        states[i][dof+7] = p.get_current_jaw_velocity()
-        states[i][dof * 2:dof * 3-1] = p.get_current_joint_effort()[0:dof-1]
-        #states[i][2 * dof + 7] = p.get_current_jaw_effort()
+        states[i][dof:dof * 2-1] = p.get_current_joint_effort()[0:dof-1]
+        states[i][dof + 7] = p.get_current_jaw_effort()
         #print('it works')
     else:
         p.move_joint_some(q[i, :], array, False)
-        states[i][0:dof] = p.get_current_joint_position()[0:dof]
-        states[i][dof:dof*2] = p.get_current_joint_velocity()[0:dof]
-        states[i][dof*2:dof*3] = p.get_current_joint_effort()[0:dof]
 
+        # states[i][0:dof] = p.get_current_joint_position()[0:dof]
+        # states[i][dof:dof*2] = p.get_current_joint_velocity()[0:dof]
+        # states[i][dof*2:dof*3] = p.get_current_joint_effort()[0:dof]
+
+        states[i][0:dof] = p.get_current_joint_position()[0:dof]
+        states[i][dof:dof * 2] = p.get_current_joint_effort()[0:dof]
 
     r.sleep()
-
     i = i + 1
 
 with open(name + '_results.csv', 'wb') as myfile:
