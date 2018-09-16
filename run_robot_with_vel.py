c60c03636dba2c35fb0841a055c92c1187bc4a39
radianag <ragondokaryono@wpi.edu>
2018-09-16 17:19:48 -0400

    collected data for two_normalize trajectory

--- run_robot_with_vel.py
+++ run_robot_with_vel.py
@@ -15,16 +15,17 @@ modelname = 'mtm'
 # modelname = 'mtm_3links_parallel'
 # modelname = 'mtm_4links_parallel'
 
-testname = 'one'
+# testname = 'one'
 # testname = 'two'
+testname = 'two_normalized'
 
 # robotname = 'PSM1'
 robotname = 'MTMR'
 
-speedscale = 0.95
+speedscale = 0.92
 scale = 0.75
 #scales = np.array([0.8, 0.8, 0.8, 1, 1, 1, 1])
-scales = np.array([0.85, 0.95, 0.95, 1, 1, 1, 1])
+scales = np.array([0.60, 0.90, 0.90, 1, 1, 1, 1])
 
 # wait for a short period of time before recording data
 stable_time = 5
